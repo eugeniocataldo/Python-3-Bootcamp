eb7293acb79542d42abc991240a7daba598224f4
Eugenio Cataldo <eu.cataldo@gmail.com>
2023-08-08 16:40:41 +0200

    Second chapter of the project
    Done all homework to the second chapter of the project

--- 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,6 +1,18 @@
+# %%
+
 from MyMainPackage.some_main_script import report_main
 from MyMainPackage.SubPackage import mysubscript
 
 report_main()
 
 mysubscript.sub_report()
+
+# %%
+
+import MyMainPackage.some_main_script as this
+# import MyMainPackage
+
+this.report_main()
+
+
+# %%
